c292a8bb789c1eea2c3591456153573178a6d7a6
Sharlena <9398483+sharlenaluyen@users.noreply.github.com>
2018-04-25 19:48:37 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,121 +1,30 @@
-var logger = require('./lib/logger');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var port = process.env.PORT || 8001;
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-var lodgings = require('./lodgings');
+const api = require('./api');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+/*
+ * Morgan is a popular logger.
+ */
+app.use(morgan('dev'));
 
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
-app.use(logger);
-
-app.get('/', function (req, res, next) {
-  res.status(200).send("Hello world!!\n");
-});
-
-app.get('/lodgings', function (req, res) {
-  console.log("  -- req.query:", req.query);
-
-  var page = parseInt(req.query.page) || 1;
-  var numPerPage = 2;
-  var lastPage = Math.ceil(lodgings.length / numPerPage);
-  page = page < 1 ? 1 : page;
-  page = page > lastPage ? lastPage : page;
-
-  var start = (page - 1) * numPerPage;
-  var end = start + numPerPage;
-  var pageLodgings = lodgings.slice(start, end);
-
-  var links = {};
-  if (page < lastPage) {
-    links.nextPage = '/lodgings?page=' + (page + 1);
-    links.lastPage = '/lodgings?page=' + lastPage;
-  }
-  if (page > 1) {
-    links.prevPage = '/lodgings?page=' + (page - 1);
-    links.firstPage = '/lodgings?page=1';
-  }
-
-  res.status(200).json({
-    lodgings: pageLodgings,
-    pageNumber: page,
-    totalPages: lastPage,
-    pageSize: numPerPage,
-    totalCount: lodgings.length,
-    links: links
-  });
-});
-
-app.post('/lodgings', function (req, res, next) {
-  console.log("  -- req.body:", req.body);
-  if (req.body && req.body.name && req.body.price) {
-    lodgings.push(req.body);
-    var id = lodgings.length - 1;
-    res.status(201).json({
-      id: id,
-      links: {
-        lodging: '/lodgings/' + id
-      }
-    });
-  } else {
-    res.status(400).json({
-      err: "Request needs a JSON body with a name and a price"
-    });
-  }
-  // console.log(lodgings);
-});
-
-function verifyLodgingID(lodgingID) {
-  return lodgingID && lodgingID >= 0 && lodgingID < lodgings.length || lodgingID === 0;
-}
-
-app.get('/lodgings/:lodgingID', function (req, res, next) {
-  console.log("  -- req.params:", req.params);
-  var lodgingID = parseInt(req.params.lodgingID);
-  if (lodgings[lodgingID]) {
-    res.status(200).json(lodgings[lodgingID]);
-  } else {
-    next();
-  }
-});
-
-app.put('/lodgings/:lodgingID', function (req, res, next) {
-  var lodgingID = parseInt(req.params.lodgingID);
-  if (lodgings[lodgingID]) {
-    if (req.body && req.body.name && req.body.price) {
-      lodgings[lodgingID] = req.body;
-      res.status(200).json({
-        links: {
-          lodging: '/lodgings/' + lodgingID
-        }
-      });
-    } else {
-      res.status(400).json({
-        err: "Request needs a JSON body with a name and a price"
-      });
-    }
-  } else {
-    next();
-  }
-  // console.log(lodgings);
-});
-
-app.delete('/lodgings/:lodgingID', function (req, res, next) {
-  console.log("  -- req.params:", req.params);
-  var lodgingID = parseInt(req.params.lodgingID);
-  if (lodgings[lodgingID]) {
-    lodgings[lodgingID] = null;
-    res.status(204).end();
-  } else {
-    next();
-  }
-  // console.log(lodgings);
-});
+/*
+ * All routes for the API are written in modules in the api/ directory.  The
+ * top-level router lives in api/index.js.  That's what we include here, and
+ * it provides all of the routes.
+ */
+app.use('/', api);
 
 app.use('*', function (req, res, next) {
   res.status(404).json({
-    err: "Path " + req.url + " does not exist"
+    error: "Requested resource " + req.originalUrl + " does not exist"
   });
 });
 
